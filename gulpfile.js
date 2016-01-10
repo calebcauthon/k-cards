6c1c7f6a28c3b742ddbf6e716f85a04749c4aeba
Caleb Cauthon <calebcauthon@gmail.com>
2016-01-11 10:26:06 +1300

    Heroku cant load shelljs

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
 var gulpNgConfig = require('gulp-ng-config');
 var child = require('child_process');
 
@@ -53,17 +52,4 @@ gulp.task('install', ['git-check'], function() {
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
 });
